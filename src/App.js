e6dc4409a41ed5bdbd57f0b2e1b51cb5a008008f
Nehaal Khandwalla <nehaalkhandwalla1@gmail.com>
2025-01-02 16:37:35 +0000

    title page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          QMFS!
+          QUEEN MARY FORMULA STUDENT PAGE
         </p>
         <a
           className="App-link"
