fd606ee1b18e2f410e2827c6c9492a450478f9d4
Nehaal Khandwalla <nehaalkhandwalla1@gmail.com>
2025-01-03 16:59:02 +0000

    main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import logo from './logo_qmfs.png';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          QUEEN MARY FORMULA STUDENT PAGE!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            QUEEN MARY FORMULA STUDENT PAGE!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; // Assuming you will style it in a separate CSS file
+import qmfslogo from './logo_qmfs.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+            <img src={qmfslogo} className="qmfs-logo-navbar" alt="logo" />
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/about">ABOUT US</Link>
+        </li>
+        <li>
+          <Link to="/about">OUR TEAM</Link>
+        </li>
+        <li>
+          <Link to="/about">OUR ACHIEVEMENTS</Link>
+        </li>
+        <li>
+          <Link to="/about">OUR CARS</Link>
+        </li>
+        <li>
+          <Link to="/about">PARTNERS</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
