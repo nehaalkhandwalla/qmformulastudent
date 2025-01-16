d080301e1974f3b120381c8f3bbe3d8753f97fec
nehaalkhandwalla <95692089+nehaalkhandwalla@users.noreply.github.com>
2025-01-16 00:01:43 +0000

    Team page content

--- src/Team.js
+++ src/Team.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './Team.css'
 
 function Team() {
     return(
     <div className="Team">
         <h2 className="Heading">
-            Meet the team!
+            Meet the Senior Team!
         </h2>
-        <p></p>
+        <p>
+        We are a collective of inspired young individuals who possess a passion for motorsports and are eager to make an impact in the future of the racing industry. The senior team works hard and prepares throughout the year to compete in Formula Student UK, where we compete against and meet teams representing universities from all over the world at the Silverstone Circuit each July to pursue various awards. 
+        </p>
     </div>
     );
 }export default Team;
