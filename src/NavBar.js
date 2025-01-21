88ebc1bdf211eb045b151785c2e30d32a940cbfe
nehaalkhandwalla <95692089+nehaalkhandwalla@users.noreply.github.com>
2025-01-21 16:11:36 +0000

    Hamburger menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; // Assuming you will style it in a separate CSS file
 import qmfslogo from './logo_qmfs.png'
 
-const Navbar = () => {
+function Navbar(){
+  const [isActive, setIsActive] = useState(false);
   return (
     <nav className="navbar" style={{ width: '100%'}}>
       <div className="navbar-logo">
             <img src={qmfslogo} className="qmfs-logo-navbar" alt="logo"/>
       </div>
-      <ul className="navbar-links">
+      <button 
+        className="hamburger" 
+        aria-label="Toggle menu" 
+        onClick={() => setIsActive(!isActive)}
+      >
+        &#9776;
+      </button>
+      <ul className={`nav-links ${isActive ? 'active' : ''}`}>
         <li>
           <Link to="/">HOME</Link>
         </li>
