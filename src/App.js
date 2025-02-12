d8fe52d630eeb2057307a2aa23753e59cb37baf9
nehaalkhandwalla <95692089+nehaalkhandwalla@users.noreply.github.com>
2025-02-12 23:11:34 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cars from './Cars';
 import Partners from './Partners';
 import Layout from './Layout';
 import Navbar from './NavBar';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/cars" element={<Cars />} />
         <Route path="/partners" element={<Partners />} />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+        <p>© 2021 Queen Mary Formula Student. All rights reserved.</p>
+        </div>
+    );
+} 
+export default Footer;
