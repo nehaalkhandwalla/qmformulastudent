6f9e3ff64e430e7e3df707ad4c784c3b10a86f2e
nehaalkhandwalla <95692089+nehaalkhandwalla@users.noreply.github.com>
2025-02-28 12:42:20 +0000

    senior photatoes

--- src/Team.js
+++ src/Team.js
@@ -18,26 +18,115 @@ function Team() {
             <div className="team-member">
             {/* from photos, show the first photo */}
             <img src={photos.Anastacia} alt="team member" className='team_member_img' />
-                <p>Anastacia Ogilvie</p>
                 <p>Team Principal</p>
+                <p>Anastacia Ogilvie</p>
+                
             </div>
             <div className="team-member">
             <img src={photos.Titouan} alt="team member" className='team_member_img' />
-                <p>Titouan Reynaud</p>
                 <p>EV Chief Engineer</p>
+                <p>Titouan Reynaud</p>
+                
             </div>
             <div className="team-member">
-                <img src="https://via.placeholder.com/150" alt="team member" id='team_member_img'/>
-                <p>John Smith</p>
+            <img src={photos.Justin} alt="team member" className='team_member_img' />
                 <p>Head of Design</p>
+                <p>John Smith</p>
+                
             </div>
             <div className="team-member">
-                <img src="https://via.placeholder.com/150" alt="team member"id='team_member_img' />
-                <p>Jane Smith</p>
+                <img src={photos.Zara} alt="team member" className='team_member_img' />
                 <p>Head of Business</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Ainiy} alt="team member" className='team_member_img' />
+                <p>Head of Marketing</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Chay} alt="team member" className='team_member_img' />
+                <p>Head of Operations</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Toto} alt="team member" className='team_member_img' />
+                <p>Head of Sponsorship</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Vlada} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Muhtasimur} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Clarica} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Dardi} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Mazeed} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Joseph} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Anton} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Ahmed} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Yaz} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Gabriel} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Mohammed} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Ryan} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Areeb} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
+            <div className="team-member">
+                <img src={photos.Riyad} alt="team member" className='team_member_img' />
+                <p>Head of Finance</p>
+                <p>John Smith</p>
+            </div>
             </div>
         </div>
-        </div>
+
     </section>
     );
 }export default Team;
--- src/assets/photos.js
+++ src/assets/photos.js
@@ -10,7 +10,12 @@ export default {
     Justin: require('./team_photos/Justin.jpg'),
     Mazeed: require('./team_photos/Mazeed.jpg'),
     Mohammed: require('./team_photos/Mohammed.jpg'),
+    Muhtasimur: require('./team_photos/Muhtasimur.jpg'),
+    Riyad: require('./team_photos/Riyad.jpg'),
     Titouan: require('./team_photos/Titouan.jpg'),
+    Toto: require('./team_photos/Toto.jpg'),
+    Vlada: require('./team_photos/Vlada.jpg'),
+    Zara: require('./team_photos/Zara.jpg'),
 }
 
   
