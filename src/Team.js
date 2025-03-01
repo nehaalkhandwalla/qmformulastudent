4b7b22b8bb1f753de1f8f7a8c64ce4dae62134f6
nehaalkhandwalla <95692089+nehaalkhandwalla@users.noreply.github.com>
2025-03-01 07:54:07 +0000

    team role colour change

--- src/Team.js
+++ src/Team.js
@@ -18,110 +18,110 @@ function Team() {
             <div className="team-member">
             {/* from photos, show the first photo */}
             <img src={photos.Anastacia} alt="team member" className='team_member_img' />
-                <p>Team Principal</p>
+                <p className='team-role'>Team Principal</p>
                 <p>Anastacia Ogilvie</p>
                 
             </div>
             <div className="team-member">
             <img src={photos.Titouan} alt="team member" className='team_member_img' />
-                <p>EV Chief Engineer</p>
+                <p className='team-role'>EV Chief Engineer</p>
                 <p>Titouan Reynaud</p>
                 
             </div>
             <div className="team-member">
             <img src={photos.Justin} alt="team member" className='team_member_img' />
-                <p>Head of Design</p>
+                <p className='team-role'>Head of Design</p>
                 <p>John Smith</p>
                 
             </div>
             <div className="team-member">
                 <img src={photos.Zara} alt="team member" className='team_member_img' />
-                <p>Head of Business</p>
+                <p className='team-role'>Head of Business</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Ainiy} alt="team member" className='team_member_img' />
-                <p>Head of Marketing</p>
+                <p className='team-role'>Head of Marketing</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Chay} alt="team member" className='team_member_img' />
-                <p>Head of Operations</p>
+                <p className='team-role'>Head of Operations</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Toto} alt="team member" className='team_member_img' />
-                <p>Head of Sponsorship</p>
+                <p className='team-role'>Head of Sponsorship</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Vlada} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Muhtasimur} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Clarica} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Dardi} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Mazeed} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Joseph} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Anton} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Ahmed} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Yaz} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Gabriel} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Mohammed} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Ryan} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Areeb} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             <div className="team-member">
                 <img src={photos.Riyad} alt="team member" className='team_member_img' />
-                <p>Head of Finance</p>
+                <p className='team-role'>Head of Finance</p>
                 <p>John Smith</p>
             </div>
             </div>
